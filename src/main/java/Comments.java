bc64b39af8c10cdbe131a76af733e9d2631d363d
Vangeli Diamants <evangelos.diamantis@web.de>
2024-07-01 16:50:25 +0200

    first commit

---
+++ src/main/java/Comments.java
@@ -0,0 +1,25 @@
+/**
+ * This is the main class.
+ */
+public class Comments {
+    /*
+
+      LICENSE
+
+    */
+
+    // Comments
+
+    /**
+     * This is the starting point of my Java program.
+     *
+     * @param args  Program argument.
+     */
+
+    /// - Item 1
+    /// - Item 2
+
+
+    public static void main(String[] args) {
+    }
+}
